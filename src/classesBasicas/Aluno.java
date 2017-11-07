06dd1075bac782db358260113f8b3f66f999c29b
Rodrigo Rodrigues <rodrigo7.carvalho.rodrigues@gmail.com>
2017-11-07 18:24:52 -0200

    Adicionada a classe Endereco e os getters e setters em Aluno e Endereco.

--- src/classesBasicas/Aluno.java
+++ src/classesBasicas/Aluno.java
@@ -1,5 +1,49 @@
 package classesBasicas;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Aluno {
 
+	@Id
+	private int id;
+	private String nome;
+	private String cpf;
+	@Embedded
+	private Endereco endereco;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
 }
---
+++ src/classesBasicas/Endereco.java
@@ -0,0 +1,54 @@
+package classesBasicas;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Endereco {
+
+	private String logradouro;
+	private String numero;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	
+	public String getLogradouro() {
+		return logradouro;
+	}
+	
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public String getRua() {
+		return rua;
+	}
+	
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	
+	public String getBairro() {
+		return bairro;
+	}
+	
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+	public String getCidade() {
+		return cidade;
+	}
+	
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+}
