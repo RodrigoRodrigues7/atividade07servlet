8d4781ef48f54a422e832acdac314f1368f2afcc
Rodrigo Rodrigues <rodrigo7.carvalho.rodrigues@gmail.com>
2017-11-08 00:49:04 -0200

    Modificações nas interfaces INegocioAluno e INegocioGenerico.

--- src/negocio/INegocioAluno.java
+++ src/negocio/INegocioAluno.java
@@ -1,5 +1,18 @@
 package negocio;
 
-public interface INegocioAluno {
+import java.util.List;
+import classesBasicas.Aluno;
 
+public interface INegocioAluno extends INegocioGenerico {
+
+	public void insert(Aluno aluno);
+	
+	public void update(Aluno aluno);
+	
+	public void remove(Aluno aluno);
+	
+	public void refresh(Aluno aluno);
+	
+	public List<Aluno> getAllByName(Aluno filtro);
+	
 }
